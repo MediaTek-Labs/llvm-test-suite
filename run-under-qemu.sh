e9c4e0fcdc87944afb799448767c5fa747c9ee30
Nikola Peric <Nikola.Peric@Syrmia.com>
2024-03-08 17:31:20 +0100

    Qemu runner script improvements
    - Add the ability to specify CPU through command line arguments
    - Increase test stack memory

--- run-under-qemu.sh
+++ run-under-qemu.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-# Invocation run_under_qemu.sh [qemu_path] [test_name] [test_args...]
+# Invocation run_under_qemu.sh [qemu_path] [cpu] [test_name] [test_args...]
 
-if [ $# -lt 2 ]; then
-    echo "Usage: run_under_qemu.sh [qemu_path] [test_name] [test_args...]"
+if [ $# -lt 3 ]; then
+    echo "Usage: run_under_qemu.sh [qemu_path] [cpu] [test_name] [test_args...]"
     exit 1
 fi
 
 # Read the qemu path
 qemu_path=$1;
 
+# Read the CPU name
+cpu=$2
+
 # Read the test name
 # This will be passed to qemu as the kernel
-test_name=$2;
+test_name=$3;
 
-# Remove the qemu path and test name from arguments
+# Remove the qemu path, cpu name and test name from arguments
 # This makes $@ expand to test arguments
-shift 2
+shift 3
 
 if [ $# -eq 0 ]; then
-    $qemu_path -cpu I7200 -semihosting -nographic -kernel $test_name 2> /dev/null
+    $qemu_path -cpu $cpu -semihosting -nographic -m 256 -kernel $test_name 2> /dev/null
 else
     # Wrapped in eval to properly expand $@
-    eval "$qemu_path -cpu I7200 -semihosting -nographic -kernel $test_name -append \"$@\"" 2> /dev/null
+    eval "$qemu_path -cpu $cpu -semihosting -nographic -m 256 -kernel $test_name -append \"$@\"" 2> /dev/null
 fi
